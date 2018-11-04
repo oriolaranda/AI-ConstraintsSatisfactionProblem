db5f6b09b9dd45078d0e834538c3907574172ed6
daniel.esquina <daniel.esquina@est.fib.upc.edu>
2018-11-04 19:15:28 +0100

    c

--- src/domini/Restriccio.java
+++ src/domini/Restriccio.java
@@ -1,4 +1,25 @@
 package domini;
+import java.util.ArrayList;
 
 public class Restriccio {
+    private String id;
+    private ArrayList<Horari> horari;  //estas cosas hay que iniciarlas en la constructora?
+
+    //Constructoras
+    public Restriccio () {
+        id=null;
+    }
+    public Restriccio (String i) {
+        id=i;
+    }
+
+    //Getters
+    public String getId(){
+        return id;
+    }
+
+    //Setters
+    public void setId(String i){
+        id=i;
+    }
 }
