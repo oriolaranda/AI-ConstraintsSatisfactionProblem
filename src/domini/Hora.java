4d3cd63ae9289f9b4f73f4f8b45ea878cbbdc9cb
alex.valls <alex.valls@est.fib.upc.edu>
2018-11-08 02:08:13 +0100

    getters i setters

--- src/domini/Hora.java
+++ src/domini/Hora.java
@@ -1,5 +1,23 @@
 package domini;
 
 public class Hora {
+    private String Dia;
+    private int Hour;
 
+
+    public String getDia() {
+        return Dia;
+    }
+
+    public void setDia(String dia) {
+        Dia = dia;
+    }
+
+    public int getHour() {
+        return Hour;
+    }
+
+    public void setHour(int hour) {
+        Hour = hour;
+    }
 }
